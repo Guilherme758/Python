eb690fdfa28b9eef82b784671a5b2268e2e652f3
Guilherme758 <68336486+Guilherme758@users.noreply.github.com>
2021-03-09 21:24:46 -0300

    Delete Exercícios/Exercícios Básicos directory

--- Exercícios/Exercícios Básicos/Exercício j.py
+++
@@ -1,9 +0,0 @@
-# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-
-celsius = float(input('Passe a temperatura em celsius: '))
-fahrenheit = 9*(celsius/5)+32
-
-if fahrenheit.is_integer() == True:
-    fahrenheit = int(fahrenheit)
-
-print("A temperatura em fahrenheit é:", f'{fahrenheit}ºF')
